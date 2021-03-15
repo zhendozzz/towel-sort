f1810b638810d52bd4f9973a90d0379eb0afc1ac
zhendozzz <info@zhendozzz.ru>
2021-03-15 18:02:22 +0300

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (Array.isArray(matrix)) {
+        const res = matrix.reduce((acc, curr, index) => {
+            sortFn = index % 2 ? (a, b) => b - a : (a, b) => a - b;
+            return acc.concat(curr.sort(sortFn));
+        }, []);
+        return res;
+    }
+    return [];
 }
